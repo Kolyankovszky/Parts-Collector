22c145d3a55069db84ff2e612babeb3dd654e249
Kolyankovszky <kolyankovszky.gusztav@gmail.com>
2024-10-31 16:26:06 +0100

    link change in productregister-inc.php. Creating a new arhiv.php file, under construction.

--- productregister-inc.php
+++ productregister-inc.php
@@ -19,19 +19,19 @@ if (isset($_POST['submit'])){
 	}
 	
 	else{	
-		$sql = "SELECT pumpmodell FROM szivattyuk WHERE pumpmodell = ?";
+		$sql = "SELECT pumpgyariszam FROM szivattyuk WHERE pumpgyariszam = ?";
 		$stmt = mysqli_stmt_init($conn);
 		if (!mysqli_stmt_prepare($stmt, $sql)) {
 			header("Location: index.php?error=sqlerror");
 			exit();
 		} else {
-			mysqli_stmt_bind_param($stmt, "s", $pumpmodell);
+			mysqli_stmt_bind_param($stmt, "s", $pumpgyariszam);
 			mysqli_stmt_execute($stmt);
 			mysqli_stmt_store_result($stmt);
 			$rowCount = mysqli_stmt_num_rows($stmt);
 			
 			if ($rowCount > 0) {
-				header("Location: index.php?error=pumpmodelltaken");
+				header("Location: index.php?error=pumpgyariszamtaken");
 				exit();
 			} else {
 				$sql = "INSERT INTO szivattyuk (pumpmarka, pumpmodell, pumpsorozat, pumpgyariszam, ugyfelid) VALUES (?, ?, ?, ?, ?)";
